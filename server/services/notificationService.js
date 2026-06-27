39809b38a19a92f30399878a122bee88c42ddd46
hasmi roohy <honeyhasmi7412@gmail.com>
2026-06-27 12:39:17 +0530

    force gmail smtp ipv4

--- server/services/notificationService.js
+++ server/services/notificationService.js
@@ -11,7 +11,10 @@ const escapeHtml = value => String(value ?? '')
   .replace(/'/g, '&#39;')
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: emailPassword
